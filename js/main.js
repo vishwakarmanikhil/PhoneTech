d6e7ea1b1bde705ed7d62e817a6c4bde69113f28
vishwakarmanikhil <menikhilv143@gmail.com>
2021-01-10 23:19:08 +0530

    1st commit
    this is first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// start registeration form validation 
+(function() {
+'use strict';
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+form.addEventListener('submit', function(event) {
+if (form.checkValidity() === false) {
+event.preventDefault();
+event.stopPropagation();
+}
+form.classList.add('was-validated');
+}, false);
+});
+}, false);
+})();
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+
+$(document).ready(function(){
+    $("#search-bar-md").on('shown.bs.modal', function(){
+        $(this).find('#search-field-md').focus();
+    });
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
